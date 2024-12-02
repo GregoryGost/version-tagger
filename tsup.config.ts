7c5e324c5c9ef4baf476ee820d4f212d486681eb
GregoryGost <huzver@gmail.com>
2024-12-02 11:01:45 +0300

    migrate to ESLint 9

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   outDir: 'dist',
   entry: ['src/index.ts'],
   target: 'esnext',
-  format: ['cjs'],
+  format: ['esm'],
   minify: true,
   treeshake: true,
   clean: true,
